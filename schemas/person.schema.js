255a9d438e3a7a8fbb14d4336fd102437cb9ad67
Phantola <phantoladev@gmail.com>
2023-10-15 23:31:53 +0900

    Add comment

--- schemas/person.schema.js
+++ schemas/person.schema.js
@@ -5,6 +5,9 @@ const personSchema = new Schema({
   name: String,
   age: Number,
   email : {type : String, required : true},
+}, {
 });
 
-module.exports = mongoose.model("Person", personSchema);
+// Person 은 단수형, people 은 복수형이므로 people 이라는 collection 이 생성됨
+// Mongoose 는 model 을 만들 때, model 이름을 소문자로 만들고 복수형으로 만든다.
+module.exports = mongoose.model("person", personSchema);
